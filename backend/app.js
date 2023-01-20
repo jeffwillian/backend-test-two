64305060a2f0c3c04feb1fccaff5d72d0d976abc
Jefferson Willian <jefferson.fechia@gmail.com>
2023-01-20 11:36:27 -0300

    feat: connection to db

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ app.use(cors())
 
 app.use(express.json())
 
+const connect = require('./db/connect')
+
+connect();
+
 app.listen(3000, function () {
-    console.log('Server Online!!!')
+    console.log('Server Online!!')
 })
---
+++ backend/db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function main() {
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongo");
+  } catch (error) {
+    console.log(`Erro: ${error}`);
+  }
+}
+
+module.exports = main;
