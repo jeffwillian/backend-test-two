ce5fcf226f268861583903e2264d9c760c101e0d
Jefferson Willian <jefferson.fechia@gmail.com>
2023-01-22 13:14:31 -0300

    refactor controllers

--- backend/controllers/breweryController.js
+++ backend/controllers/breweryController.js
@@ -1,5 +1,31 @@
-const { Brewery: brewerySchema }= require("../models/brewery");
+const mongoose = require("mongoose");
+const breweryModel = require("../models/Brewery.js");
+const connection = require("../db/connect");
+const Brewery = mongoose.model("Brewery", breweryModel);
 
-const breweryController = {};
+class BreweryController {
+  async create(req, res) {
+    const data = req.body;
+    try {
+      var result = await Brewery.create(data);
+      res.status(201).json({ result, msg: "Sucessfully created" });
+    } catch (error) {
+      res.status(400).json({ result, msg: "Bad request" });
+    }
+  }
 
-module.exports = serviceController;
+  async index(req, res) {
+    res.status(200).json({ todo: true });
+  }
+  async update(req, res) {
+    res.status(200).json({ todo: true });
+  }
+  async delete(req, res) {
+    res.status(200).json({ todo: true });
+  }
+  async findById(req, res) {
+    res.status(200).json({ todo: true });
+  }
+}
+
+module.exports = new BreweryController();
--- backend/routes/brewery.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-
-const breweryController = require("../controllers/breweryController");
-
-//Functions
-
-module.exports = router;
